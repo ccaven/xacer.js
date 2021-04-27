0c80d49321022772f34be203389238efc4878334
xacer <chacecaven@outlook.com>
2021-04-26 20:54:26 -0400

    Added more functions to mat4

--- src/math/mat4.js
+++ src/math/mat4.js
@@ -1,4 +1,7 @@
 
+/**
+ * Stores a 4x4 matrix
+ */
 export class Matrix4 extends Float32Array {
 
     /**
@@ -131,4 +134,223 @@ export class Matrix4 extends Float32Array {
         return this;
     }
 
+    /**
+     * Transpose a 4x4 matrix
+     * @returns {Matrix4} reference to original
+     */
+    transpose () {
+        let a01 = this[1],
+            a02 = this[2],
+            a03 = this[3];
+        let a12 = this[6],
+            a13 = this[7];
+        let a23 = this[11];
+        this[1] = this[4];
+        this[2] = this[8];
+        this[3] = this[12];
+        this[4] = a01;
+        this[6] = this[9];
+        this[7] = this[13];
+        this[8] = a02;
+        this[9] = a12;
+        this[11] = this[14];
+        this[12] = a03;
+        this[13] = a13;
+        this[14] = a23;
+        return this;
+    }
+
+    /**
+     * Inverts a 4x4 matrix
+     * @returns {Matrix4} reference to original
+     */
+    invert () {
+        let a00 = this[0],
+            a01 = this[1],
+            a02 = this[2],
+            a03 = this[3];
+        let a10 = this[4],
+            a11 = this[5],
+            a12 = this[6],
+            a13 = this[7];
+        let a20 = this[8],
+            a21 = this[9],
+            a22 = this[10],
+            a23 = this[11];
+        let a30 = this[12],
+            a31 = this[13],
+            a32 = this[14],
+            a33 = this[15];
+
+        let b00 = a00 * a11 - a01 * a10;
+        let b01 = a00 * a12 - a02 * a10;
+        let b02 = a00 * a13 - a03 * a10;
+        let b03 = a01 * a12 - a02 * a11;
+        let b04 = a01 * a13 - a03 * a11;
+        let b05 = a02 * a13 - a03 * a12;
+        let b06 = a20 * a31 - a21 * a30;
+        let b07 = a20 * a32 - a22 * a30;
+        let b08 = a20 * a33 - a23 * a30;
+        let b09 = a21 * a32 - a22 * a31;
+        let b10 = a21 * a33 - a23 * a31;
+        let b11 = a22 * a33 - a23 * a32;
+
+        // Calculate the determinant
+        let det = b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
+
+        if (!det) return null;
+
+        det = 1.0 / det;
+
+        this[0] = (a11 * b11 - a12 * b10 + a13 * b09) * det;
+        this[1] = (a02 * b10 - a01 * b11 - a03 * b09) * det;
+        this[2] = (a31 * b05 - a32 * b04 + a33 * b03) * det;
+        this[3] = (a22 * b04 - a21 * b05 - a23 * b03) * det;
+        this[4] = (a12 * b08 - a10 * b11 - a13 * b07) * det;
+        this[5] = (a00 * b11 - a02 * b08 + a03 * b07) * det;
+        this[6] = (a32 * b02 - a30 * b05 - a33 * b01) * det;
+        this[7] = (a20 * b05 - a22 * b02 + a23 * b01) * det;
+        this[8] = (a10 * b10 - a11 * b08 + a13 * b06) * det;
+        this[9] = (a01 * b08 - a00 * b10 - a03 * b06) * det;
+        this[10] = (a30 * b04 - a31 * b02 + a33 * b00) * det;
+        this[11] = (a21 * b02 - a20 * b04 - a23 * b00) * det;
+        this[12] = (a11 * b07 - a10 * b09 - a12 * b06) * det;
+        this[13] = (a00 * b09 - a01 * b07 + a02 * b06) * det;
+        this[14] = (a31 * b01 - a30 * b03 - a32 * b00) * det;
+        this[15] = (a20 * b03 - a21 * b01 + a22 * b00) * det;
+        return this;
+    }
+
+    /**
+     * Calculate the adjugate of a matrix
+     * @returns {Matrix4} reference to original
+     */
+    adjoint () {
+        let a00 = this[0],
+            a01 = this[1],
+            a02 = this[2],
+            a03 = this[3];
+        let a10 = this[4],
+            a11 = this[5],
+            a12 = this[6],
+            a13 = this[7];
+        let a20 = this[8],
+            a21 = this[9],
+            a22 = this[10],
+            a23 = this[11];
+        let a30 = this[12],
+            a31 = this[13],
+            a32 = this[14],
+            a33 = this[15];
+        this[0] =
+            a11 * (a22 * a33 - a23 * a32) -
+            a21 * (a12 * a33 - a13 * a32) +
+            a31 * (a12 * a23 - a13 * a22);
+        this[1] = -(
+            a01 * (a22 * a33 - a23 * a32) -
+            a21 * (a02 * a33 - a03 * a32) +
+            a31 * (a02 * a23 - a03 * a22)
+        );
+        this[2] =
+            a01 * (a12 * a33 - a13 * a32) -
+            a11 * (a02 * a33 - a03 * a32) +
+            a31 * (a02 * a13 - a03 * a12);
+        this[3] = -(
+            a01 * (a12 * a23 - a13 * a22) -
+            a11 * (a02 * a23 - a03 * a22) +
+            a21 * (a02 * a13 - a03 * a12)
+        );
+        this[4] = -(
+            a10 * (a22 * a33 - a23 * a32) -
+            a20 * (a12 * a33 - a13 * a32) +
+            a30 * (a12 * a23 - a13 * a22)
+        );
+        this[5] =
+            a00 * (a22 * a33 - a23 * a32) -
+            a20 * (a02 * a33 - a03 * a32) +
+            a30 * (a02 * a23 - a03 * a22);
+        this[6] = -(
+            a00 * (a12 * a33 - a13 * a32) -
+            a10 * (a02 * a33 - a03 * a32) +
+            a30 * (a02 * a13 - a03 * a12)
+        );
+        this[7] =
+            a00 * (a12 * a23 - a13 * a22) -
+            a10 * (a02 * a23 - a03 * a22) +
+            a20 * (a02 * a13 - a03 * a12);
+        this[8] =
+            a10 * (a21 * a33 - a23 * a31) -
+            a20 * (a11 * a33 - a13 * a31) +
+            a30 * (a11 * a23 - a13 * a21);
+        this[9] = -(
+            a00 * (a21 * a33 - a23 * a31) -
+            a20 * (a01 * a33 - a03 * a31) +
+            a30 * (a01 * a23 - a03 * a21)
+        );
+        this[10] =
+            a00 * (a11 * a33 - a13 * a31) -
+            a10 * (a01 * a33 - a03 * a31) +
+            a30 * (a01 * a13 - a03 * a11);
+        this[11] = -(
+            a00 * (a11 * a23 - a13 * a21) -
+            a10 * (a01 * a23 - a03 * a21) +
+            a20 * (a01 * a13 - a03 * a11)
+        );
+        this[12] = -(
+            a10 * (a21 * a32 - a22 * a31) -
+            a20 * (a11 * a32 - a12 * a31) +
+            a30 * (a11 * a22 - a12 * a21)
+        );
+        this[13] =
+            a00 * (a21 * a32 - a22 * a31) -
+            a20 * (a01 * a32 - a02 * a31) +
+            a30 * (a01 * a22 - a02 * a21);
+        this[14] = -(
+            a00 * (a11 * a32 - a12 * a31) -
+            a10 * (a01 * a32 - a02 * a31) +
+            a30 * (a01 * a12 - a02 * a11)
+        );
+        this[15] =
+            a00 * (a11 * a22 - a12 * a21) -
+            a10 * (a01 * a22 - a02 * a21) +
+            a20 * (a01 * a12 - a02 * a11);
+        return this;
+    }
+
+    /**
+     * Calculate the determinant of a matrix
+     * @returns {Number} the determinant
+     */
+    determinant () {
+        let a00 = this[0],
+            a01 = this[1],
+            a02 = this[2],
+            a03 = this[3];
+        let a10 = this[4],
+            a11 = this[5],
+            a12 = this[6],
+            a13 = this[7];
+        let a20 = this[8],
+            a21 = this[9],
+            a22 = this[10],
+            a23 = this[11];
+        let a30 = this[12],
+            a31 = this[13],
+            a32 = this[14],
+            a33 = this[15];
+        let b00 = a00 * a11 - a01 * a10;
+        let b01 = a00 * a12 - a02 * a10;
+        let b02 = a00 * a13 - a03 * a10;
+        let b03 = a01 * a12 - a02 * a11;
+        let b04 = a01 * a13 - a03 * a11;
+        let b05 = a02 * a13 - a03 * a12;
+        let b06 = a20 * a31 - a21 * a30;
+        let b07 = a20 * a32 - a22 * a30;
+        let b08 = a20 * a33 - a23 * a30;
+        let b09 = a21 * a32 - a22 * a31;
+        let b10 = a21 * a33 - a23 * a31;
+        let b11 = a22 * a33 - a23 * a32;
+        // Calculate the determinant
+        return b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
+    }
 }
